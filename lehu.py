0337a7aa8ad50eb3024349e15f310a5dc82c7657
mangogao <jjmangogao@gmail.com>
2017-03-29 14:42:16 +0800

    Remove unnecessary codes

--- lehu.py
+++ lehu.py
@@ -3,21 +3,17 @@ import click
 from core.api import LehuPicker
 from core.utils import load_data, print_head
 
-def do_download(file_name, force):
-    data = load_data(file_name)
+@click.command()
+@click.option('--data', default=None, help='Name of data file.')
+@click.option('-f', is_flag=True, help='Override existing files.')
+def do_download(data, f):
+    data = load_data(data)
     picker = LehuPicker()
     for item in data:
         print_head(item['caption'])
         filelist = picker.get_filelist(item['code'])
         for file in filelist:
-            picker.download_file(file[0], item['dir'], file[1], force)
-
-@click.command()
-@click.option('--data', default=None, help='Name of data file.')
-@click.option('-f', is_flag=True, help='Override existing files.')
-def main(data, f):
-    do_download(data, f)
-
+            picker.download_file(file[0], item['dir'], file[1], f)
 
 if __name__ == '__main__':
-    main()
+    do_download()

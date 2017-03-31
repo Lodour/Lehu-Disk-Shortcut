2826d498a8a3d6d82f72f1793f7e7521dae40636
mangogao <jjmangogao@gmail.com>
2017-03-31 10:47:53 +0800

    Add DocString & Reformat

--- lehu.py
+++ lehu.py
@@ -3,17 +3,24 @@ import click
 from core.api import LehuPicker
 from core.utils import load_data, print_head
 
+
 @click.command()
 @click.option('--data', default=None, help='Name of data file.')
 @click.option('-f', is_flag=True, help='Override existing files.')
 def do_download(data, f):
+    """ Execute download action.
+    :param data: Path of the data file
+    :param f: Should override existing files or not 
+    :return: None
+    """
     data = load_data(data)
     picker = LehuPicker()
     for item in data:
         print_head(item['caption'])
-        filelist = picker.get_filelist(item['code'])
-        for file in filelist:
+        file_list = picker.get_filelist(item['code'])
+        for file in file_list:
             picker.download_file(file[0], item['dir'], file[1], f)
+
 
 if __name__ == '__main__':
     do_download()

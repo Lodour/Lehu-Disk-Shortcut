cf281f4cbb259f08b6b8e63c46fac1e8b8bd97f4
mangogao <jjmangogao@gmail.com>
2017-03-31 11:02:11 +0800

    Remove unused variables

--- core/api.py
+++ core/api.py
@@ -18,7 +18,6 @@ class LehuBase(requests.Session):
         # Set url of index page and parse it to an element tree
         self.index_url = index_url or 'http://disk.lehu.shu.edu.cn/'
         self.index_tree = etree.HTML(self.get(self.index_url).text)
-        self.index_hrefs = self.index_tree.xpath(r'//a')
 
     def post_form(self, tree, url, data):
         """ Post data to url with `input` fields in tree

3369b7af15957f8d5ded4b280c90544c28c765a2
mangogao <jjmangogao@gmail.com>
2017-04-01 16:26:18 +0800

    Fix command line tools

--- lehu.py
+++ lehu.py
@@ -4,10 +4,15 @@ from core.api import LehuPicker, LehuUploader
 from core.utils import load_data, print_head
 
 
-@click.command()
+@click.group()
+def cli1():
+    pass
+
+
+@cli1.command()
 @click.option('--data', default=None, help='Name of data file.')
 @click.option('-f', is_flag=True, help='Override existing files.')
-def do_download(data, f):
+def pull(data, f):
     """ Execute download action.
     :param data: Path of the data file
     :param f: Should override existing files or not 
@@ -21,11 +26,16 @@ def do_download(data, f):
             picker.download_file(file[0], item['dir'], file[1], f)
 
 
-@click.command()
+@click.group()
+def cli2():
+    pass
+
+
+@cli2.command()
 @click.option('--code', help='Upload code.')
 @click.option('--path', help='Path to upload file.')
 @click.option('--name', help='Name of upload file.')
-def do_upload(code, path, name):
+def push(code, path, name):
     """ Execute upload action.
     :param code: where to upload
     :param path: path to upload file
@@ -35,5 +45,7 @@ def do_upload(code, path, name):
     print uploader.upload_file(code, path, name)
 
 
+cli = click.CommandCollection(sources=[cli1, cli2])
+
 if __name__ == '__main__':
-    do_download()
+    cli()
